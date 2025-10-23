b464514da2447577cbd6fb4af28141e01ac0ff88
alvinson-tech <alvinson.tech@gmail.com>
2025-10-23 10:42:09 +0530

    Basic Recursion

---
+++ Basic Recursion/basic.cpp
@@ -0,0 +1,21 @@
+// Time to start with HIS favourtie topic - Recursion!
+// The main concept is to give you concept! - Raj :)
+// DSA IS ALL ABOUT DSA - Raj Again :)
+
+#include<iostream>
+using namespace std;
+int cnt = 0;
+
+void printName() {
+    if(cnt == 5)
+        return;
+    
+    cnt++;
+    cout << "Alvin is Great! 😎" << endl;
+    cout << "No kidding.. 🤭" << endl;
+    printName();
+}
+
+int main() {
+    printName();
+}
